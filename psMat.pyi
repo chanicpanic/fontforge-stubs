229abdc5cee598d2c07d9d4f9a0369d4d474dab1
chanicpanic <51764816+chanicpanic@users.noreply.github.com>
2025-09-13 16:20:11 -0700

    Review psMat

--- psMat.pyi
+++ psMat.pyi
@@ -2,33 +2,31 @@
 Provides quick access to some useful transformations expressed as PostScript matrices
 """
 
-from typing import Tuple
-
-def identity() -> Tuple[float, float, float, float, float, float]:
+def identity() -> tuple[float, float, float, float, float, float]:
     """
     Returns an identity matrix as a 6 element tuple.
     """
     ...
 
 def compose(
-    mat1: Tuple[float, float, float, float, float, float],
-    mat2: Tuple[float, float, float, float, float, float],
-) -> Tuple[float, float, float, float, float, float]:
+    mat1: tuple[float, float, float, float, float, float],
+    mat2: tuple[float, float, float, float, float, float],
+) -> tuple[float, float, float, float, float, float]:
     """
     Returns a matrix which is the composition of the two input transformations.
     """
     ...
 
 def inverse(
-    mat: Tuple[float, float, float, float, float, float],
-) -> Tuple[float, float, float, float, float, float]:
+    mat: tuple[float, float, float, float, float, float],
+) -> tuple[float, float, float, float, float, float]:
     """
     Returns a matrix which is the inverse of the input transformation.
     (Note: There will not always be an inverse)
     """
     ...
 
-def rotate(theta: float) -> Tuple[float, float, float, float, float, float]:
+def rotate(theta: float) -> tuple[float, float, float, float, float, float]:
     """
     Returns a matrix which will rotate by `theta`. `theta` is expressed in radians.
     """
@@ -36,7 +34,7 @@ def rotate(theta: float) -> Tuple[float, float, float, float, float, float]:
 
 def scale(
     x: float, y: float | None = None
-) -> Tuple[float, float, float, float, float, float]:
+) -> tuple[float, float, float, float, float, float]:
     """
     Returns a matrix which will scale by `x` in the horizontal direction and
     `y` in the vertical. If `y` is omitted, it will scale by the same
@@ -44,14 +42,14 @@ def scale(
     """
     ...
 
-def skew(theta: float) -> Tuple[float, float, float, float, float, float]:
+def skew(theta: float) -> tuple[float, float, float, float, float, float]:
     """
     Returns a matrix which will skew by `theta` (to produce an oblique font).
     `theta` is expressed in radians.
     """
     ...
 
-def translate(x: float, y: float) -> Tuple[float, float, float, float, float, float]:
+def translate(x: float, y: float) -> tuple[float, float, float, float, float, float]:
     """
     Returns a matrix which will translate by `x` in the horizontal direction
     and `y` in the vertical.
