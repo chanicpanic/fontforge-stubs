1662d75d2c10eefbadbf5976b2c485627d98d8c2
chanicpanic <51764816+chanicpanic@users.noreply.github.com>
2025-09-29 21:46:20 -0700

    test struct

--- tests/run_stubtest.py
+++ tests/run_stubtest.py
@@ -1,9 +1,11 @@
 import site
 import sys
 
-site.addsitedir(sys.argv[1])
+import struct
 
-from mypy.stubtest import test_stubs, parse_options
+# site.addsitedir(sys.argv[1])
 
-exit_code = test_stubs(parse_options(["fontforge", "psMat", "--ignore-missing-stub"]))
-sys.exit(exit_code)
+# from mypy.stubtest import test_stubs, parse_options
+
+# exit_code = test_stubs(parse_options(["fontforge", "psMat", "--ignore-missing-stub"]))
+# sys.exit(exit_code)
